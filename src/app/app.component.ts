ab9e3394c511785b1e0f3813339788aa1078da6c
lucianogalvani <luciano.galvani@gmail.com>
2016-12-23 10:27:40 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>My app with Webpack</h1>
+        `
+})
+export class AppComponent { }
